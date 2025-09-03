3d8f5db9bc2ab985bce0b92319400cf39611a62d
Dev-Caua-Ricken <cauaricken@gmail.com>
2025-09-03 20:36:22 -0300

    prova

--- src/main/java/Prova/atividade4/ContaBancaria.java
+++ src/main/java/Prova/atividade4/ContaBancaria.java
@@ -1,9 +1,19 @@
 package atividade4;
 
 public class ContaBancaria {
-    String titular;
-    double saldo;
+    private String titular;
+    private double saldo;
 
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 
     public void depositar(double valor){
         saldo += valor;
--- src/main/java/Prova/atividade4/main.java
+++ src/main/java/Prova/atividade4/main.java
@@ -4,9 +4,11 @@ public class main {
     public static void main(String[] args) {
 
         ContaBancaria conta = new ContaBancaria();
-        conta.titular = "cauã";
+        conta.setTitular("caua");
+        System.out.println("o titular da conta é " + conta.getTitular());
         conta.depositar(100);
         conta.sacar(25);
+        System.out.println("saldo final é de " + conta.getSaldo());
        }
     }
 
