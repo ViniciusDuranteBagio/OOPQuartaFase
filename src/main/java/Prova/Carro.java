7f4219853cb3461431230419428872444741ddb5
Felippe Stange Blasius <blasiusfelippe@gmail.com>
2025-09-21 15:24:24 -0300

    resolvendo comflitos para puxar atualizações da master

--- src/main/java/Prova/Carro.java
+++ src/main/java/Prova/Carro.java
@@ -1,12 +1,10 @@
 package Prova;
-
 public class Carro {
+    public String marca;
+    public int ano;
 
-    public String marca ;
-    public int ano ;
-
-    public void exibirDados(){
-        System.out.println("Marca: " + marca + "\nAno: " + ano);
-
+    public void exibirDados() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Ano: " + ano);
     }
 }
